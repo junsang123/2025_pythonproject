534b25d159b6bd17036981a639fddcd3184555e3
junsang123 <junsangcho@icloud.com>
2025-11-01 11:23:09 +0900

    a

--- if.py
+++ if.py
@@ -1,11 +1,25 @@
+from os.path import split
+
+from w import user_input
+
 score=int(input('입력'))
 
-if score <=100:
-    if score>=0:
-        if score >= 80:
+if score > 100 or score <0:
+    print("ㄴㄴ")
+else:
+    if score >= 80:
             print('ㅇ')
-        elif score >= 60:
+    elif score >= 60:
             print("re")
-        else:
+    else:
             print('ㄴ')
+while True:
+    user_input("입력")
+    if user_input.lower()=="z":
+        break
 
+input_number=int(input("입력"))
+index=1
+while index < input_number:
+    print(index)
+    index+=1
