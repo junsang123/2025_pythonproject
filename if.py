8c0e971d2ae53b7dd11c90e259ac6bbfb4e90ecc
junsang123 <junsangcho@icloud.com>
2025-11-01 11:09:35 +0900

    a

---
+++ if.py
@@ -0,0 +1,11 @@
+score=int(input('입력'))
+
+if score <=100:
+    if score>=0:
+        if score >= 80:
+            print('ㅇ')
+        elif score >= 60:
+            print("re")
+        else:
+            print('ㄴ')
+
