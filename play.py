3ee858cda2c7800acad578ac5af6ab7fb3e79b9c
junsang123 <junsangcho@icloud.com>
2025-11-29 11:55:14 +0900

    if

--- play.py
+++ play.py
@@ -1,7 +1,6 @@
 import random
 
 a = random.randint(1, 100)
-90
 
 def down():
     while True:
@@ -13,6 +12,22 @@ def down():
                 print("d")
             elif b == a:
                 break
-down()
+import time
+
+def t():
+    while True:
+        s = time.time()
+        input()
+        e=time.time()
+        a = e - s
+        if 9.7<=a<=10.3:
+            print("성공")
+            break
+        else:
+            print("실패")
+            break
+
+
+t()
 
 
